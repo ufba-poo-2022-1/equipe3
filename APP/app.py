cb7c2a207931da61c3fca3486aabeca7b4fc3de1
Ian Ramos <ianramossantos@hotmail.com>
2022-05-25 18:42:34 -0300

    integrate app with sqlalchemy

--- APP/app.py
+++ APP/app.py
@@ -1,25 +1,24 @@
-from flask import (
-    Flask, 
-    jsonify
-)
+from flask import Flask, jsonify
+from database import db
 
-# Function that create the app 
-def create_app(test_config=None ):
+# Function that create the app
+def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://database.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Simple route
-    @app.route('/')
-    def hello_world(): 
-        return jsonify({
-           "status": "success",
-            "message": "Hello World!"
-        }) 
-     
-    return app # do not forget to return the app
+    @app.route("/")
+    def hello_world():
+        return jsonify({"status": "success", "message": "Hello World!"})
+
+    return app  # do not forget to return the app
+
 
 APP = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    db.init_app(APP)
     # APP.run(host='0.0.0.0', port=5000, debug=True)
     APP.run(debug=True)
