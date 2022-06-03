e0ab28c94f769024a1ca9f80d76047afaf044983
Ian Ramos <ianramossantos@hotmail.com>
2022-06-03 18:46:49 -0300

    feat: create address model

---
+++ app/models/address_model.py
@@ -0,0 +1,13 @@
+from app import db
+
+
+class Address(db.Model):
+    __tablename__ = "adresses"
+
+    id = db.Column(db.Integer, primary_key=True)
+    street = db.Column(db.String(128))
+    number = db.Column(db.Integer)
+    district = db.Column(db.String(128))
+    city = db.Column(db.String(128))
+    cep = db.Column(db.String(128))
+    complement = db.Column(db.String(128))
