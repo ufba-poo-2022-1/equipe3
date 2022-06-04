691533691d0a11e8dc4b4147b9384f2b28a0cd7d
Augusto Barreto <72322635+augustobarreto@users.noreply.github.com>
2022-06-04 10:57:28 -0300

    Add Post Parameter

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -2,11 +2,12 @@ from datetime import datetime
 
 
 """ Run anomaly detection inference based on interval passed in database.ini file """
-def add_user():
+def add_user(id):
     print('\n\n\n\n\n###########')
     print('{} - Script starting'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
     print('###########\n\n\n\n\n')
     print('Adding the user into database\n\n\n\n\n')
+    print(id)
     print('\n\n\n\n\n###########')
     print('{} - Script ending'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
     print('###########\n\n\n\n\n')
