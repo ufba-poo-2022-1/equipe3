d6f783bbcc85a99d36e546e67cb5440032a0377c
Ian Ramos <ianramossantos@hotmail.com>
2022-06-11 20:25:39 -0300

    refactor: set users id to uuid
    - It is no longer necessary to pass the id of each entity as a parameter in the constructor, it is instantiated automaticallyt
    - The uuid is safer and avoid conflicts and problems betweens ids.

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -1,6 +1,6 @@
-import json
 from datetime import datetime
-from models.user_model import User, db
+from extensions import db
+from models.user_model import User
 
 
 """ Controller responsible for adding and deleting user endpoints """
@@ -11,16 +11,16 @@ def add_user(json_data):
     print("{} - Script starting".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
     print("###########\n\n\n\n\n")
     print("Adding the user into database\n\n\n\n\n")
-    id = json_data["id"]
     name = json_data["name"]
     email = json_data["email"]
     password = json_data["password"]
     phone = json_data["phone"]
 
-    user = User(id, name, email, password, phone)
+    user = User(name, email, password, phone)
 
     db.session.add(user)
     db.session.commit()
+
     # j = jsonify(i=id, n=name,e=email,p=password,ph=phone,t=type)
     # res = j
     # res = list(map(lambda x: json.loads(x), res))
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,48 +1,25 @@
+from uuid import uuid4
+
 from extensions import db
 
 
 class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String, primary_key=True)
     name = db.Column(db.String(128))
     email = db.Column(db.String(128), unique=True)
     password = db.Column(db.String(128))
     phone = db.Column(db.String(120))
-    type = db.Column(db.String(128))
+    type = db.Column(db.String(128))  # necessário para a herança
 
     address = db.relationship("Address", backref="users")
 
-    __mapper_args__ = {"polymorphic_identity": "user", "polymorphic_on": type}
+    __mapper_args__ = {"polymorphic_identity": "users", "polymorphic_on": type}
 
-    def __init__(self, id, name, email, password, phone):
-        self.id = id
+    def __init__(self, name, email, password, phone):
+        self.id = str(uuid4())
         self.name = name
         self.email = email
         self.password = password
         self.phone = phone
-
-    def get_id(self):
-        return self.id
-
-    def get_data(self):
-        return [
-            self.__id,
-            self.__name,
-            self.__email,
-            self.__password,
-            self.__celphone,
-            self.__address,
-        ]
-
-    def set_cellphone(self, number):
-        self.__celphone = number
-        return 0
-
-    def set_email(self, email):
-        self.__email = email
-        return 0
-
-    def set_password(self, password):
-        self.__password = password
-        return 0
