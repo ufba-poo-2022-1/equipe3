2c616561fd562b49905dfcd97e62326b592e023f
Ian Ramos <ianramossantos@hotmail.com>
2022-06-11 20:48:59 -0300

    feat: create constructor for owner class

--- app/models/owner_model.py
+++ app/models/owner_model.py
@@ -5,7 +5,19 @@ from .user_model import User
 class Owner(User):
     __tablename__ = "owners"
 
-    id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+    id = db.Column(db.String, db.ForeignKey("users.id"), primary_key=True)
+
+    # TODO: criar relacionamento com a classe de de escritura, que irá extender
+    # de documentos
     deed_id = db.Column(db.Integer)
 
     __mapper_args__ = {"polymorphic_identity": "owners"}
+
+    def __init__(self, name, email, password, phone, deed_id):
+        super().__init__(
+            name,
+            email,
+            password,
+            phone,
+        )
+        self.deed_id = deed_id
