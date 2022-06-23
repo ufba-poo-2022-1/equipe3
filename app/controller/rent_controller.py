a3b7f3ba017519789c97facadbd53d55dda2f14c
Ian Ramos <ianramossantos@hotmail.com>
2022-06-23 13:48:35 -0300

    feat: update service to create a rent

--- app/controller/rent_controller.py
+++ app/controller/rent_controller.py
@@ -1,24 +1,73 @@
-import json
 from datetime import datetime
-from models.rent_model import Rent, db
+
+from extensions import db
+from models.rent_model import Rent
+from models.immobile_model import Immobile
+from shared.app_errors import AppError
 
 
 """ Controller responsible for adding rents endpoints """
+
+
 def add_rent(json_data):
+    owner_id = json_data["owner_id"]
+    immobile_id = json_data["immobile_id"]
+    tenant_id = json_data["tenant_id"]
 
-    start_date = json_data['start_date']
-    end_date = json_data['end_date']
-    expected_return_date = json_data['expected_return_date']
-    total = json_data['total']
-    fine_amount = json_data['fine_amount']
-    owner_id = json_data['owner_id']
-    immobile_id = json_data['immobile_id']
-    tenant_id = json_data['tenant_id']
+    try:
+        start_date = datetime.strptime(json_data["start_date"], "%Y/%m/%d %H:%M")
+        expected_return_date = datetime.strptime(
+            json_data["expected_return_date"], "%Y/%m/%d %H:%M"
+        )
 
-    rent = Rent(start_date,end_date,expected_return_date,total,fine_amount,owner_id,immobile_id,tenant_id)
+    except ValueError as error:
+        raise AppError("A data está em um formato inválido.", description=error.__str__())
 
-    db.session.add(rent)
-    db.session.commit()
+    minimum_rent_hour = 24
+
+    rent_open_to_immobile = Rent.find_open_rent_by_imombile(immobile_id)
+    rent_open_to_tenant = Rent.find_open_rent_by_tenant(tenant_id)
+    rent_open_to_owner = Rent.find_open_rent_by_owner(owner_id)
+
+    if rent_open_to_immobile:
+        raise AppError("Já existe um aluguel em aberto para esse imóvel.")
+
+    if rent_open_to_tenant:
+        raise AppError("Já existe um aluguel em aberto para esse inquilino.")
+
+    if rent_open_to_owner:
+        raise AppError("Já existe um aluguel em aberto para esse proprietário.")
+
+    date_now = start_date or datetime.now()
+    compare = (expected_return_date - date_now).total_seconds() // 3600
+
+    if compare < minimum_rent_hour:
+        raise AppError("Data de retorno inválida.")
+
+    rent = Rent(
+        start_date,
+        expected_return_date,
+        owner_id,
+        immobile_id,
+        tenant_id,
+    )
+
+    immobile = Immobile.find_by_id(immobile_id)
+
+    immobile.is_available = False
+
+    try:
+        db.session.add(rent)
+        db.session.add(immobile)
+        db.session.commit()
+
+    except Exception as error:
+        raise AppError(
+            "Não foi possível cadastrar um aluguel",
+            description=error.__str__(),
+        )
+
+    return rent.transform_to_json()
 
 
 def cancel_rent(json_data):
