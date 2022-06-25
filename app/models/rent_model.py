18fa4d258176f34e5c2b224bf5bc1feefc50614f
asb21 <asb2@cesar.org.br>
2022-06-25 11:21:39 -0300

    Rent Model DocumentatioN

--- app/models/rent_model.py
+++ app/models/rent_model.py
@@ -4,6 +4,27 @@ from extensions import db
 
 
 class Rent(db.Model):
+    """
+    Classe que faz a herança da classe base Model e define a entidade Rent.
+
+    Parameters
+    ----------
+    id: string
+    start_date:datetime
+    end_date:datetime
+    expected_return_date:datetime
+    total:float
+    owner_id:string
+    immobile_id:string
+    tenant_id:string
+
+    Returns
+    -------
+     response: __type__
+        Entidade Rent criada
+
+    """
+
     __tablename__ = "rents"
 
     id = db.Column(db.String, primary_key=True)
@@ -33,18 +54,76 @@ class Rent(db.Model):
 
     @classmethod
     def find_by_id(cls, rent_id):
+        """
+        Método responsável por buscar um aluguel em determinada classe por seu id
+
+        Parameters
+        ----------
+        rent_id: string
+        cls:__type__
+
+        Returns
+        -------
+        response: __type__
+        Entidade filtrada por seu id
+
+        """
+        
         return db.session.query(cls).filter_by(id=rent_id).first()
 
     @classmethod
     def find_by_tenant_id(cls, tenant_id):
+        """
+        Método responsável por buscar um aluguel em determinada classe por seu tenant_id
+
+        Parameters
+        ----------
+        tenant_id: string
+        cls:__type__
+
+        Returns
+        -------
+        response: __type__
+        Entidade filtrada por seu id
+
+        """
+
         return db.session.query(cls).filter_by(tenant_id=tenant_id).all()
 
     @classmethod
     def find_by_owner_id(cls, owner_id):
+        """
+        Método responsável por buscar um aluguel em determinada classe por seu owner_id
+
+        Parameters
+        ----------
+        owner_id: string
+        cls:__type__
+
+        Returns
+        -------
+        response: __type__
+        Entidade filtrada por seu id
+
+        """
         return db.session.query(cls).filter_by(owner_id=owner_id).all()
 
     @classmethod
     def find_open_rent_by_imombile(cls, immobile_id):
+        """
+        Método responsável por buscar um aluguel em aberto por seu immobile_id
+
+        Parameters
+        ----------
+        immobile_id: string
+        cls:__type__
+
+        Returns
+        -------
+        response: __type__
+        Entidade filtrada por seu id
+
+        """
         return (
             db.session.query(cls)
             .filter_by(immobile_id=immobile_id, end_date=None)
@@ -53,10 +132,38 @@ class Rent(db.Model):
 
     @classmethod
     def find_open_rent_by_tenant(cls, tenant_id):
+        """
+        Método responsável por buscar um aluguel em em aberto por seu tenant_id
+
+        Parameters
+        ----------
+        tenant_id: string
+        cls:__type__
+
+        Returns
+        -------
+        response: __type__
+        Entidade filtrada por seu id
+
+        """
         return db.session.query(cls).filter_by(tenant_id=tenant_id, end_date=None).first()
 
     @classmethod
     def find_open_rent_by_owner(cls, owner_id):
+        """
+        Método responsável por buscar um aluguel em em aberto por seu owner_id
+
+        Parameters
+        ----------
+        owner_id: string
+        cls:__type__
+
+        Returns
+        -------
+        response: __type__
+        Entidade filtrada por seu id
+
+        """
         return db.session.query(cls).filter_by(owner_id=owner_id, end_date=None).first()
 
     def transform_to_json(self):
